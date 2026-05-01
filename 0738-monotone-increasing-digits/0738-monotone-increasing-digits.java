1ab4bcc054124cdeff6226ead68acb8d7f06e52c
Prachi Mehar <134136816+prachimehar@users.noreply.github.com>
2026-05-01 11:25:00 +0530

    Time: 1 ms (90.29%), Space: 42.2 MB (60.63%) - LeetHub

---
+++ 0738-monotone-increasing-digits/0738-monotone-increasing-digits.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int monotoneIncreasingDigits(int n) {
+        char[] digits = String.valueOf(n).toCharArray();
+
+        int marker = digits.length;
+
+        for(int i = digits.length -1; i> 0; i--){
+            if(digits[i] < digits[i-1]){
+                digits[i-1]--;
+                marker = i;
+            }
+        }
+
+        for(int i = marker; i< digits.length; i++){
+            digits[i] = '9';
+        }
+
+        return Integer.parseInt(new String(digits));
+    }
+}
