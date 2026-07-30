64447534e3c0709033500d076a66b85a32a75b0e
Prachi Mehar <134136816+prachimehar@users.noreply.github.com>
2026-07-30 20:38:38 +0530

    Time: 2 ms (82.95%), Space: 43.9 MB (32.91%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+
+        int[] count = new int[26];
+
+        for(char ch : s.toCharArray()){
+            count[ch - 'a']++;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        boolean[] visited = new boolean[26];
+
+
+        for(char ch : s.toCharArray()){
+
+            count[ch - 'a']--;
+
+            if(visited[ch - 'a']){
+                continue;
+            }
+
+            while(!stack.isEmpty() 
+                  && stack.peek() > ch 
+                  && count[stack.peek() - 'a'] > 0){
+
+                char removed = stack.pop();
+                visited[removed - 'a'] = false;
+            }
+
+            stack.push(ch);
+            visited[ch - 'a'] = true;
+        }
+
+
+        StringBuilder result = new StringBuilder();
+
+        while(!stack.isEmpty()){
+            result.append(stack.pop());
+        }
+
+        return result.reverse().toString();
+    }
+}
