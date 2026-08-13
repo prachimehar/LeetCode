0e6ef61378be78c764896caf5a4c19a4c71d644b
Prachi Mehar <134136816+prachimehar@users.noreply.github.com>
2026-08-13 13:04:27 +0530

    Time: 1 ms (77.51%), Space: 42.6 MB (34.48%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isHappy(int n) {
+
+        HashSet<Integer> seen = new HashSet<>();
+
+        while (n != 1) {
+            if (seen.contains(n)) {
+                return false;
+            }
+            seen.add(n);
+            n = getSumOfSquares(n);
+        }
+
+        return true;
+    }
+
+    private int getSumOfSquares(int n) {
+
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n = n / 10;
+        }
+
+        return sum;
+    }
+}
